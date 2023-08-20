7adc0341bcc671c87cc0237f56f54b922b126cdc
Andre Naef <andre@naef.com>
2023-08-20 16:44:28 +0200

    remove reference to undefined variable

--- examples/modules/example.lua
+++ examples/modules/example.lua
@@ -2,7 +2,7 @@ _ENV = setmetatable({ }, { __index = _G })
 
 -- Returns a a message with the current time
 function get_time ()
-	return string.format("It is %s.", os.date(nil, NOW))
+	return string.format("It is %s.", os.date())
 end
 
 -- Return module
